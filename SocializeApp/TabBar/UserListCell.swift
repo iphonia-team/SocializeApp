8500a32258a4faff4126bd8104da8f5df4a977ab
Whyeon728 <80697051+Whyeon728@users.noreply.github.com>
2022-04-12 01:31:00 +0900

    유저리스트뷰 완성 - 유저리스트셀에 이미지 삽입시 자동으로 원형 이미지로 변경 - 유저리스트셀에 국가코드 데이터 입력시 자동으로  해당 국기 이모지 디스플레이 - 유저리스트셀 클릭 이벤트 발생시 Alert 메시지 출력 - Alert 메시지에서 1:1 채팅 클릭시 개인 채팅방으로 뷰 이동

---
+++ SocializeApp/TabBar/UserListCell.swift
@@ -0,0 +1,36 @@
+//
+//  UserListCell.swift
+//  SocializeApp
+//
+//  Created by Whyeon on 2022/04/12.
+//
+
+import UIKit
+
+class UserListCell: UITableViewCell {
+    @IBOutlet weak var userListImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        self.userListImageView.layer.cornerRadius = (self.userListImageView.frame.size.height)/2
+//        self.userListImageView.clipsToBounds = true
+//    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
