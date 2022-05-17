3b35186da5e5259b785d29586cd25bb7273e6b3a
Whyeon728 <80697051+Whyeon728@users.noreply.github.com>
2022-05-17 22:00:47 +0900

    -1

--- SocializeApp/TabBar/ChatListViewController.swift
+++ SocializeApp/TabBar/ChatListViewController.swift
@@ -12,50 +12,51 @@ import FirebaseFirestore
 class ChatListViewController: UIViewController {
 
     @IBOutlet weak var chatListTableView: UITableView!
-    
+    let database = Firestore.firestore()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.chatListTableView.delegate = self
         self.chatListTableView.dataSource = self
     }
     
-    func getMessageList() {
-        print("@@!!getMessageList")
-        database.collection("chatRooms").addSnapshotListener{ snapshot, error in
-            self.comments = []
-            debugPrint("----------------------")
-            if let err = error {
-                print("Error writing document: \(err.localizedDescription)")
-            } else {
-                guard let documents = snapshot?.documents else { return }
-                for doc in documents {
-                    if (doc.documentID == self.chatRoomUid) {
-                        guard let data = doc.data()["comments"] as? [[String: Any]] else {
-                            print("not Convert!!!")
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
-                            }
-                            return
-                            
-                        }
-                        print("doc.data()[comments]: \(data)")
-
-                        for index in data {
-                            guard let uid = index["uid"] as? String else { return }
-                            guard let message = index["message"] as? String else { return }
-                            guard let date = index["date"] as? String else { return }
-                            self.comments.append(Comment(uid: uid, message: message, date: date))
-                        }
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
-                            self.tableView.scrollToRow(at: IndexPath(row: self.comments.count-1, section: 0), at: .top, animated: false)
-                        }
-                        print("@@@@@self.comments: \(self.comments[0])")
-                    }
-                }
-            }
-        }
-    }
+//    func getMessageList() {
+//        print("@@!!getMessageList")
+//        database.collection("chatRooms").addSnapshotListener{ snapshot, error in
+//            self.comments = []
+//            debugPrint("----------------------")
+//            if let err = error {
+//                print("Error writing document: \(err.localizedDescription)")
+//            } else {
+//                guard let documents = snapshot?.documents else { return }
+//                for doc in documents {
+//                    if (doc.documentID == self.chatRoomUid) {
+//                        guard let data = doc.data()["comments"] as? [[String: Any]] else {
+//                            print("not Convert!!!")
+//                            DispatchQueue.main.async {
+//                                self.tableView.reloadData()
+//                            }
+//                            return
+//
+//                        }
+//                        print("doc.data()[comments]: \(data)")
+//
+//                        for index in data {
+//                            guard let uid = index["uid"] as? String else { return }
+//                            guard let message = index["message"] as? String else { return }
+//                            guard let date = index["date"] as? String else { return }
+//                            self.comments.append(Comment(uid: uid, message: message, date: date))
+//                        }
+//                        DispatchQueue.main.async {
+//                            self.tableView.reloadData()
+//                            self.tableView.scrollToRow(at: IndexPath(row: self.comments.count-1, section: 0), at: .top, animated: false)
+//                        }
+//                        print("@@@@@self.comments: \(self.comments[0])")
+//                    }
+//                }
+//            }
+//        }
+//    }
         
 }
 
